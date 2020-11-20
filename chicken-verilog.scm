80c629904e24d3e1f18e8c9e431eb9a565c6d416
Hiroki Yagi <hiro.yagi0245@gmail.com>
2020-11-20 18:50:06 +0900

    module

--- chicken-verilog.scm
+++ chicken-verilog.scm
@@ -1,1 +1,9 @@
-(define )
+(import (chicken irregex))
+
+(define (hello x) 12)
+
+(define (kebab->snake str)
+  (irregex-replace/all "-" str "_"))
+
+(define (v/module name )
+  `(module ,expr endmodule))
