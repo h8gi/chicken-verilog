e32cf006452199089968d4a3b148a6dee22ca8b1
Hiroki Yagi <hiro.yagi0245@gmail.com>
2020-12-18 17:52:25 +0900

    add syntax-rules

--- chicken-verilog.scm
+++ chicken-verilog.scm
@@ -12,3 +12,22 @@
 
 
 (define-class <verilog-module> () ())
+
+(defmodule (my-module)
+  (description "DESCRIPTION")
+  (interface
+   (output 8 result)
+   (input 8 c-clk))
+  (wire 8 test-wire)
+  (always
+   (@ (posedge i-clk))
+   (<= a 12)
+   (foo bar)))
+
+(define-syntax defmodule
+  (syntax-rules (description interface input output)
+    [(_ (description expression)
+        (interface
+         ())
+        body...)
+     ()]))
