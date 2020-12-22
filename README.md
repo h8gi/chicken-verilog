c3570d1ec3f820487a4afd497aa2f10598185e7b
yagi <hiro.yagi0245@gmail.com>
2020-12-22 10:12:48 +0900

    links

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+## links
+
 - http://wiki.call-cc.org/eggs%20tutorial
 - http://wiki.call-cc.org/man/5/Extensions
 - http://www.nongnu.org/geiser/geiser_2.html#Installation
--- chicken-verilog.scm
+++ chicken-verilog.scm
@@ -7,3 +7,15 @@
 
 (define (v/module name )
   `(module ,expr endmodule))
+
+(hello 12)
+
+(define-verilog-module uart
+  #:input clk rst rx
+  #:output hello (reg [7 0] foo)
+  (parameter x 9600)
+  (parameter y 10000)
+  (reg )
+)
+
+mediocore
